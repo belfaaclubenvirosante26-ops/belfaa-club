d7280bb39ee5191790038907844920989fb154b4
belfaaclubenvirosante26-ops <belfaaclub.envirosante26@gmail.com>
2026-09-20 20:03:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.querySelector('.menu')?.addEventListener('click',()=>document.querySelector('nav').classList.toggle('open'));
+const menu=document.querySelector('.menu');
+const nav=document.querySelector('nav');
+if(menu && nav){
+  menu.addEventListener('click',()=>nav.classList.toggle('open'));
+  nav.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
+}
